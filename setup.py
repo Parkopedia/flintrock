99047949554e59165a8b65d2609ce0dc4bf9116e
mblack <martin@parkopedia.com>
2021-08-16 12:23:51 +0100

    Rename package as ppds-flintrock

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='Flintrock',
+    name='ppds-flintrock',
     version=__version__,
     description='A command-line tool for launching Apache Spark clusters.',
     long_description=long_description,
