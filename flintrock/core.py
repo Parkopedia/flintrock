bc96b8849ba2bea2b34d62aa3dd1aeab95c8b00d
mblack <martin@parkopedia.com>
2021-08-16 12:24:54 +0100

    Pin java patch version

--- flintrock/core.py
+++ flintrock/core.py
@@ -526,11 +526,13 @@ def ensure_java8(client: paramiko.client.SSHClient):
 
         ssh_check_output(
             client=client,
+
             command="""
                 set -e
 
                 # Install Java 1.8 first to protect packages that depend on Java from being removed.
-                sudo yum install -y java-1.8.0-openjdk
+                # Pin to specific patch version of 1.8 that is known to be compatible with Spark
+                sudo yum install -y java-1.8.0-openjdk-1.8.0.161-0.b14.amzn2.x86_64
 
                 # Remove any older versions of Java to force the default Java to 1.8.
                 # We don't use /etc/alternatives because it does not seem to update links in /usr/lib/jvm correctly,
